cd6262dfaa4015ad24ae0e2701f63715f4b3c34f
Efthymios Maneas <efthymios.maneas.14@ucl.ac.uk>
2014-11-10 17:23:29 +0000

    Creation a function inside the main program for the boids initialization

--- boids.py
+++ boids.py
@@ -7,41 +7,50 @@ from matplotlib import pyplot as plt
 from matplotlib import animation
 import random
 
-# Deliberately terrible code for teaching purposes
 
-boids_number = 50
+def initialize_boids(boids_number):
+    boids_x=[]
+    boids_y=[]
+    boid_x_velocities=[]
+    boid_y_velocities=[]
 
-boids_x=[random.uniform(-450,50.0) for x in range(boids_number)]
-boids_y=[random.uniform(300.0,600.0) for x in range(boids_number)]
-boid_x_velocities=[random.uniform(0,10.0) for x in range(boids_number)]
-boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(boids_number)]
-boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
+    for x in range(boids_number):
+        boids_x.append(random.uniform(-450,50.0))
+        boids_y.append(random.uniform(300.0,600.0))
+        boid_x_velocities.append(random.uniform(0,10.0))
+        boid_y_velocities.append(random.uniform(-20.0,20.0))
+
+    return boids_x, boids_y, boid_x_velocities, boid_y_velocities
+
+
+boids = initialize_boids(50)    # Setting up the initial number of boids
+
 
 def update_boids(boids):
-	xs,ys,xvs,yvs=boids
-	# Fly towards the middle
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			xvs[i]=xvs[i]+(xs[j]-xs[i])*0.01/len(xs)
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			yvs[i]=yvs[i]+(ys[j]-ys[i])*0.01/len(xs)
-	# Fly away from nearby boids
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:
-				xvs[i]=xvs[i]+(xs[i]-xs[j])
-				yvs[i]=yvs[i]+(ys[i]-ys[j])
-	# Try to match speed with nearby boids
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 10000:
-				xvs[i]=xvs[i]+(xvs[j]-xvs[i])*0.125/len(xs)
-				yvs[i]=yvs[i]+(yvs[j]-yvs[i])*0.125/len(xs)
-	# Move according to velocities
-	for i in range(len(xs)):
-		xs[i]=xs[i]+xvs[i]
-		ys[i]=ys[i]+yvs[i]
+    xs,ys,xvs,yvs=boids
+    # Fly towards the middle
+    for i in range(len(xs)):
+        for j in range(len(xs)):
+            xvs[i]=xvs[i]+(xs[j]-xs[i])*0.01/len(xs)
+    for i in range(len(xs)):
+        for j in range(len(xs)):
+            yvs[i]=yvs[i]+(ys[j]-ys[i])*0.01/len(xs)
+    # Fly away from nearby boids
+    for i in range(len(xs)):
+        for j in range(len(xs)):
+            if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:
+                xvs[i]=xvs[i]+(xs[i]-xs[j])
+                yvs[i]=yvs[i]+(ys[i]-ys[j])
+    # Try to match speed with nearby boids
+    for i in range(len(xs)):
+        for j in range(len(xs)):
+            if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 10000:
+                xvs[i]=xvs[i]+(xvs[j]-xvs[i])*0.125/len(xs)
+                yvs[i]=yvs[i]+(yvs[j]-yvs[i])*0.125/len(xs)
+    # Move according to velocities
+    for i in range(len(xs)):
+        xs[i]=xs[i]+xvs[i]
+        ys[i]=ys[i]+yvs[i]
 
 
 figure=plt.figure()
