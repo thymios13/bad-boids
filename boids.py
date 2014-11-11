ab90f45457bf84e986cd79725bae7b99a6a1ef91
Efthymios Maneas <efthymios.maneas.14@ucl.ac.uk>
2014-11-11 23:29:04 +0000

    Change some variable names for better code understanding

--- boids.py
+++ boids.py
@@ -10,18 +10,18 @@ import random
 
 
 def initialize_boids(boids_number):
-    boids_x=[]
-    boids_y=[]
+    boids_x_positions=[]
+    boids_y_positions=[]
     boid_x_velocities=[]
     boid_y_velocities=[]
 
     for x in range(boids_number):
-        boids_x.append(random.uniform(-450,50.0))
-        boids_y.append(random.uniform(300.0,600.0))
+        boids_x_positions.append(random.uniform(-450,50.0))
+        boids_y_positions.append(random.uniform(300.0,600.0))
         boid_x_velocities.append(random.uniform(0,10.0))
         boid_y_velocities.append(random.uniform(-20.0,20.0))
 
-    return boids_x, boids_y, boid_x_velocities, boid_y_velocities
+    return boids_x_positions, boids_y_positions, boid_x_velocities, boid_y_velocities
 
 # Number and positions/velocities of boids initialization
 bois_number = 50
@@ -29,25 +29,25 @@ boids = initialize_boids(bois_number)
 
 
 def update_boids(boids):
-    xs,ys,xvs,yvs=boids
-    boids_number = len(xs)
+    x_positions,y_positions,x_velocities,y_velocities=boids
+    boids_number = len(x_positions)
     for i in range(bois_number):
         for j in range(bois_number):
             # Fly towards the middle
-            xvs[i]+=(xs[j]-xs[i])*0.01/boids_number
-            yvs[i]+=(ys[j]-ys[i])*0.01/boids_number
+            x_velocities[i]+=(x_positions[j]-x_positions[i])*0.01/boids_number
+            y_velocities[i]+=(y_positions[j]-y_positions[i])*0.01/boids_number
             # Fly away from nearby boids
-            tmp_velocity = power((xs[j]-xs[i]),2) + power((ys[j]-ys[i]),2)
+            tmp_velocity = power((x_positions[j]-x_positions[i]),2) + power((y_positions[j]-y_positions[i]),2)
             if tmp_velocity < 100:
-                xvs[i]+=(xs[i]-xs[j])
-                yvs[i]+=(ys[i]-ys[j])
+                x_velocities[i]+=(x_positions[i]-x_positions[j])
+                y_velocities[i]+=(y_positions[i]-y_positions[j])
             # Try to match speed with nearby boids
             elif tmp_velocity < 10000:
-                xvs[i]+=(xvs[j]-xvs[i])*0.125/boids_number
-                yvs[i]+=(yvs[j]-yvs[i])*0.125/boids_number
+                x_velocities[i]+=(x_velocities[j]-x_velocities[i])*0.125/boids_number
+                y_velocities[i]+=(y_velocities[j]-y_velocities[i])*0.125/boids_number
         # Move according to velocities
-        xs[i]+=xvs[i]
-        ys[i]+=yvs[i]
+        x_positions[i]+=x_velocities[i]
+        y_positions[i]+=y_velocities[i]
 
 
 figure=plt.figure()
