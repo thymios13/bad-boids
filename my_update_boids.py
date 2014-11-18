c51a01c86b354813ef402e7fde05908b1e145b5c
Efthymios Maneas <efthymios.maneas.14@ucl.ac.uk>
2014-11-18 22:14:59 +0000

    Some of these are not used any more -> object oriented approach

--- my_update_boids.py
+++ my_update_boids.py
@@ -1,21 +1,25 @@
 from boids_velocity import tmp_velocity
 
 def update_boids(boids):
-    x_positions,y_positions,x_velocities,y_velocities=boids
+    [x_positions, y_positions, x_velocities, y_velocities] = boids
     boids_number = len(x_positions)
+
     for i in range(boids_number):
         for j in range(boids_number):
             # Fly towards the middle
-            x_velocities[i]+=(x_positions[j]-x_positions[i])*0.01/boids_number
-            y_velocities[i]+=(y_positions[j]-y_positions[i])*0.01/boids_number
+            x_velocities[i] += (x_positions[j]-x_positions[i])*0.01/boids_number
+            y_velocities[i] += (y_positions[j]-y_positions[i])*0.01/boids_number
+
             # Fly away from nearby boids
             if tmp_velocity(x_positions,y_positions,i,j) < 100:
-                x_velocities[i]+=(x_positions[i]-x_positions[j])
-                y_velocities[i]+=(y_positions[i]-y_positions[j])
+                x_velocities[i] += (x_positions[i]-x_positions[j])
+                y_velocities[i] += (y_positions[i]-y_positions[j])
+
             # Try to match speed with nearby boids
             elif tmp_velocity(x_positions,y_positions,i,j) < 10000:
-                x_velocities[i]+=(x_velocities[j]-x_velocities[i])*0.125/boids_number
-                y_velocities[i]+=(y_velocities[j]-y_velocities[i])*0.125/boids_number
+                x_velocities[i] += (x_velocities[j]-x_velocities[i])*0.125/boids_number
+                y_velocities[i] += (y_velocities[j]-y_velocities[i])*0.125/boids_number
+
         # Move according to velocities
         x_positions[i]+=x_velocities[i]
         y_positions[i]+=y_velocities[i]
